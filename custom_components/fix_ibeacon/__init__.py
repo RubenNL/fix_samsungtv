e73347535def12910e9f8066c125660ea8d6773a
Ruben <ruben@rubend.nl>
2022-11-02 15:42:57 +0100

    fix another copy/paste mistake.

--- custom_components/fix_ibeacon/__init__.py
+++ custom_components/fix_ibeacon/__init__.py
@@ -9,7 +9,7 @@ DOMAIN = "fix_ibeacon"
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     with open("/usr/src/homeassistant/homeassistant/components/ibeacon/device_tracker.py", "a") as myfile:
-      myfile.write('\n    @property\n    def state(self) -> str:\n        """Return the state of the device."""\n        return self._ibeacon_advertisement.source if self._active else STATE_NOT_HOME\n"')
+      myfile.write('\n    @property\n    def state(self) -> str:\n        """Return the state of the device."""\n        return self._ibeacon_advertisement.source if self._active else STATE_NOT_HOME\n')
     with open("/usr/src/homeassistant/homeassistant/components/ibeacon/const.py", "a") as myfile:
       myfile.write('\nUNAVAILABLE_TIMEOUT = 30\n')
     return True
