1187db2a76938831a702ec018360fd57d4adb175
Ruben <ruben@rubend.nl>
2022-11-02 18:19:54 +0100

    added UPDATE_INTERVAL

--- custom_components/fix_ibeacon/__init__.py
+++ custom_components/fix_ibeacon/__init__.py
@@ -11,5 +11,5 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     with open("/usr/src/homeassistant/homeassistant/components/ibeacon/device_tracker.py", "a") as myfile:
       myfile.write('\n    @property\n    def state(self) -> str:\n        """Return the state of the device."""\n        return self._ibeacon_advertisement.source if self._active else STATE_NOT_HOME\n')
     with open("/usr/src/homeassistant/homeassistant/components/ibeacon/const.py", "a") as myfile:
-      myfile.write('\nUNAVAILABLE_TIMEOUT = 30\n')
+      myfile.write('\nUNAVAILABLE_TIMEOUT = 30\nUPDATE_INTERVAL = timedelta(seconds=10)\n')
     return True
